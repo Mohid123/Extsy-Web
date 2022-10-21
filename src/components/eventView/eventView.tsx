b64c57fdebbd758a92be73bfec34c40664f5b790
zubairsalarzai <zubairsalar.hrp@gmail.com>
2022-10-20 22:33:00 -0700

    minor error fixed

--- src/components/eventView/eventView.tsx
+++ src/components/eventView/eventView.tsx
@@ -1,8 +1,8 @@
 import UpcomingEvents from '../eventPosts/upcomingEvents/upcomingEvents';
 import GroupAdminLeftOption from '../groups/groupAdmin/groupAdminLeftOption';
-import Ad from '../mainViewComponents/ad';
-import FriendSuggestion from '../mainViewComponents/friendSuggestion';
-import ProfileInfo from '../mainViewComponents/profileInfo';
+import Ad from '../mainViewComponents/mainViewRight/ad';
+import FriendSuggestion from '../mainViewComponents/mainViewRight/friendSuggestion';
+import ProfileInfo from '../mainViewComponents/mainViewLeft/profileInfo';
 import './eventView.scss';
 import EventViewCard from './eventViewCard/eventViewCard';
 
